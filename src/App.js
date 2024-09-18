c5d9e3f0f0216fe05eea8da4b3e087b651ce98ca
tiyo5151 <s1F102400224@iniad.org>
2024-09-18 17:53:47 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
+import Card from './components/Card/card';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon/';
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [prevUrl, setPrevUrl] = useState('');
+  const [nextUrl, setNextUrl] = useState('');
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //すべてのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      //各ポケモンの詳細なデータを取得
+      // console.log(res);
+      loadPokemon(res.results);
+      // console.log(res.results);
+      // console.log(res);
+      setPrevUrl(res.previous);
+      setNextUrl(res.next);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  // console.log(pokemonData);
+
+  const handlePrevPage = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    // console.log(data);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+  const handleNextPage = async () => {
+    if (!nextUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    // console.log(data);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>loading...</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Card/card.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './card.css';
+
+const Card = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} alt="" />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div key={type.type.name}>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">高さ：{pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">重さ：{pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">
+            アビリティ：
+            {pokemon.abilities.map((ability) => {
+              return (
+                <span key={ability.ability.name}>{ability.ability.name}</span>
+              );
+            })}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
